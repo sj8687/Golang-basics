5f4aa95848245d83c338940cb255ac77c24e375d
sj8687 <shreyashjadhav59807@gmail.com>
2026-07-06 18:33:34 +0530

    added values and varibles

---
+++ 3_Varibles/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+
+	// infer
+	// var name = "golang"
+	// var isAdult bool = true
+
+	// var age int = 30
+
+	// shorthand syntax
+	// name := "golang"
+
+	var name string
+	name = "golang"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	// price := 50.5
+
+	fmt.Println(name)
+}
